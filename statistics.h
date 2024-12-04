56eef95d996790c5b85e3c8bafe2ca68286f617d
hzmzk <hazim_zaki96@yahoo.com>
2024-12-04 13:37:19 +0800

    new file:   statistics.h

---
+++ statistics.h
@@ -0,0 +1,22 @@
+#ifndef STATISTICS
+#define STATISTICS
+
+#include <random>
+#include <chrono>
+
+namespace rng 
+{
+    inline std::random_device rd{};
+
+    inline std::seed_seq ss{
+        static_cast<unsigned int>(std::chrono::steady_clock::now().time_since_epoch().count()),
+        rd(), rd() };
+
+    inline std::mt19937 mt{ ss };
+}
+
+
+
+
+
+#endif
